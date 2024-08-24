a7744f723325b9870abdf3115981ddb98eb45efc
ReituTheCompSciGirlie <nobuhlereitumetse@gmail.com>
2024-08-25 00:41:31 +0200

    changed Infantry

--- Infantry.h
+++ Infantry.h
@@ -1,28 +1,15 @@
 #ifndef INFANTRY_H
 #define INFANTRY_H
 
+#include "UnitComponent.h"
 
-#include "LegionUnit.h"
-#include <iostream>
-#include <string>
-using namespace std;
-
-
-class Infantry : public LegionUnit 
+class Infantry : public LegionUnit, UnitComponent
 {
-    private:
-       int power; // this is for attacking 
-       std::string formation;
 
-     public:
-        Infantry(int health, int defence, int power, std::string formation);
-        void move()override;
-        void attack()override;
-        void changeFormation(std::string& newFormation);
-        void defend(); //Im trying to creative here please dont mind me 
-        void retreat();
-        std::string getType() const override;
-
+    // functions to override from UnitComponent
+public:
+    void move() override;
+    void fight() override;
 };
 
-#endif 
+#endif INFANTRY_H
